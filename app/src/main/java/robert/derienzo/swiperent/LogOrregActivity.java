2e9edabbfe7148cbe7303994a54cddee06a144f1
Robert DeRienzo <Robert.DeRienzo@knights.ucf.edu>
2019-08-06 18:02:10 -0400

    Add user auth + Login page + Register page

--- app/src/main/java/robert/derienzo/swiperent/LogOrregActivity.java
+++ app/src/main/java/robert/derienzo/swiperent/LogOrregActivity.java
@@ -1,13 +1,42 @@
 package robert.derienzo.swiperent;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LogOrregActivity extends AppCompatActivity {
 
+    private Button mLogin, mRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_orreg);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogOrregActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogOrregActivity.this, RegistractionActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
     }
-}
+    }
+
